d6734bfb9e09289df9daa95996ddd680631685a6
sergio23p <76039944+sergio23p@users.noreply.github.com>
2021-08-14 10:19:40 +0100

    Update mainOpenBCIStreaming.m

--- mainOpenBCIStreaming.m
+++ mainOpenBCIStreaming.m
@@ -3,10 +3,15 @@ clear;
 clc;
 
 %% Train
-cfgProtoc = [];
-cfgPreproc = [];
+listaFicheros = {'1_close_left_eye_1.csv', '1_close_left_eye_2.csv', '1_close_left_eye_3.csv','1_close_left_eye_4.csv', '1_close_eyes_1.csv', '1_close_eyes_2.csv','1_close_eyes_3.csv', '1_close_eyes_4.csv', '1_close_right_eye_1.csv', '1_close_right_eye_2.csv', '1_close_right_eye_3.csv', '1_close_right_eye_4.csv','1_close_left_eye_1.csv', '1_close_left_eye_2.csv', '1_close_left_eye_3.csv','1_close_left_eye_4.csv'};
 
-trainDataset = Dataset(cfgProtoc, cfgPreproc);
+markers = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
+
+trainDataset = Dataset();
+
+for i = 1 : length(markers)
+     trainDataset = trainDataset.addTrial(listaFicheros{i}, markers{i});   
+ end
 
 % Add the data to the trainDataset class
 

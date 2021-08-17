c13860ada304a4afabd9645fde94d5451b07071d
sergio23p <76039944+sergio23p@users.noreply.github.com>
2021-08-17 15:17:59 +0100

    Update mainOpenBCIStreaming.m

--- mainOpenBCIStreaming.m
+++ mainOpenBCIStreaming.m
@@ -18,7 +18,12 @@ for i = 1 : length(markers)
 %% Classifier
 secs = 2;
 
+datos_filtrados = {myDataset.data_filtered(1:160).filtered_data};
+
+Label = [ones(1,40) (repmat (2,1,40)) (repmat (3,1,40)) (repmat (4,1,40))];
+
+
 realTime = StreamingOpenBCI(secs, 125);
 
-realTime = realTime.train(trainDataset);
+realTime = realTime.train(Datos_filtrados{i}, Label(i));
 % realTime = realTime.classifyStreamingSim();

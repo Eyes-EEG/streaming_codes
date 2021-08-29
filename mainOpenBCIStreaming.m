34f9a71cb42dd21e3a17f272401957ca3ac2fdd5
sergio23p <76039944+sergio23p@users.noreply.github.com>
2021-08-29 22:10:29 +0100

    Update mainOpenBCIStreaming.m

--- mainOpenBCIStreaming.m
+++ mainOpenBCIStreaming.m
@@ -1,8 +1,8 @@
-%% Clear
+% Clear
 clear;
 clc;
 
-%% Train
+% Train
 listaFicheros = {'1_close_left_eye_1.csv', '1_close_left_eye_2.csv', '1_close_left_eye_3.csv','1_close_left_eye_4.csv', '1_close_eyes_1.csv', '1_close_eyes_2.csv','1_close_eyes_3.csv', '1_close_eyes_4.csv', '1_close_right_eye_1.csv', '1_close_right_eye_2.csv', '1_close_right_eye_3.csv', '1_close_right_eye_4.csv','1_close_left_eye_1.csv', '1_close_left_eye_2.csv', '1_close_left_eye_3.csv','1_close_left_eye_4.csv'};
 
 markers = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4};
@@ -15,7 +15,7 @@ for i = 1 : length(markers)
 
 % Add the data to the trainDataset class
 
-%% Classifier
+% Classifier
 secs = 2;
 
 Datos_filtrados = {trainDataset.data_filtered(1:160).filtered_data};

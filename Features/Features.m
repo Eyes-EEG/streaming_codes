d9871137bfdf9985eab352f515d7491cfac212da
sergio23p <76039944+sergio23p@users.noreply.github.com>
2021-09-04 18:10:15 +0100

    Update Features.m

--- Features/Features.m
+++ Features/Features.m
@@ -90,7 +90,7 @@ classdef Features
         end
         
         function featTable = generateFeatures (obj,data_structure)
-            data = data_structure;
+            data = data_structure{1};
             CH1 = data (1,:);
             canal1 = obj.CalculaValorAlfa(CH1);
             obj.matriz_alfa(length(obj.matriz_alfa)+1).canal1 = canal1;
